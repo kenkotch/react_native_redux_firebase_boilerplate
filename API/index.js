8d329f10d455606c67f151aa4bbb77878ee9d30c
Ken Kotch <hello@ken.uno>
2018-03-06 08:52:29 -0700

    fix API/index.js

--- API/index.js
+++ API/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 
 const app = express()
 
-require('./routes/authRoutes')(app)
+const authRoutes = require('./routes/authRoutes')
+
+app.use('/authRoutes', authRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
